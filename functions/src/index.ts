c59e11a3373a0cbfd5b55df2588dfe68ddc3ad9e
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2019-11-29 17:17:43 -0500

    [ADD] Added assynchronous call

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,85 +7,71 @@ import * as functions from 'firebase-functions';
 //  response.send("Hello from Firebase!");
 // });
 
-//import fetch from 'node-fetch';
-//import * as cheerio from 'cheerio';
+// import fetch from 'node-fetch';
+// import * as cheerio from 'cheerio';
 
 // Google Assistant deps
 import { dialogflow, SimpleResponse, BasicCard, Button, Image } from 'actions-on-google';
 const app = dialogflow({ debug: true});
 
-// Odoo XML-RPC ts https://www.npmjs.com/package/odoo-xmlrpc
+//Odoo XML-RPC ts https://www.npmjs.com/package/odoo-xmlrpc
 
 const Odoo = require('odoo-xmlrpc');
 
 const odoo = new Odoo({
     url: 'https://lgharib-odoo-assistant.odoo.com/',
-    port: '80',
+    port: '443',
     db: 'lgharib-odoo-assistant-master-726081',
     username: 'larbizard@gmail.com',
     password: 'odooassistant'
 });
 
 
-
 // Capture Intent
-app.intent('Create a lead', async (conv) => {
-
-	const data = await createLead();
-	if( data != '0'){
+app.intent('The name of the lead is', async (conv, {any}) => {
+	return createLead(String(any)).then(function(data) {
 		conv.close(new SimpleResponse({
-			text: 'Lead created',
-			speech: 'The lead was created', 
+			text: `Lead ${any} created`,
+			speech: `The lead ${any} was created`, 
 		}));
 		conv.close(new BasicCard({
-	                text: 'Lead created',
-		        image: new Image({
-				url: 'https://kioteservices.com/wp-content/uploads/2017/12/odoo_logo.png',
-				alt: 'Odoo Logo',
+				text: `Lead ${any} created`,
+				image: new Image({
+				url: `https://kioteservices.com/wp-content/uploads/2017/12/odoo_logo.png`,
+				alt: `Odoo Logo`,
 			}),
 			buttons: new Button({
-				title: 'Lead link',
-				url: 'https://staging.gesteve.umontreal.ca/',
+				title: `Open ${any} lead`,
+				url: `https://lgharib-odoo-assistant.odoo.com/web?#id=${String(data)}&action=168&model=crm.lead&view_type=form&cids=1&menu_id=121`,
 			})
-
-		}));	
-	}
-	else{
-		conv.close(new SimpleResponse({
-			text: 'Error creating lead see logs ',
-			speech: 'Error creating lead see logs', 
 		}));
-	}
-
+	}).catch(function(){
+		conv.close(new SimpleResponse({
+			text: `Error`,
+			speech: `Error`, 
+		}));
+	});
+		
 });
 
-// Helper Function for scrapping a webpage
-async function createLead() {
-	let result = '';
-	odoo.connect(function (error:any) {
-		if (error) { 
-			console.log(error);
-			result = '0';
-		}
-		else{
-		    console.log('Connected to Odoo server.');
-		    const inParams = [];
-		    inParams.push({'name': 'New Lead Savoir-Faire Linux'})
-		    const params = [];
-		    params.push(inParams);
-		    odoo.execute_kw('crm.lead', 'create', params, function (err:any, value:any) {
-		        if (err) { 
-		        	console.log(err);
-		        }
-		        else {
-					console.log('Result: ', value);
-		        	result = value;
-		        }
-		    });
-		}
-	});
-	return result;
 
+// Helper Function for scrapping a webpage
+function createLead(any:string) {
+	return new Promise((resolve, reject)=>{
+		odoo.connect(  function (error:any) {
+			if (error) { console.log(error); }
+			console.log('Connected to Odoo server.');
+			const inParams = [];
+			inParams.push({'name': any})
+			const params = [];
+			params.push(inParams);
+			odoo.execute_kw('crm.lead', 'create', params, function (err:any, value:any) {
+				if (err) { console.log(err); }
+				console.log('Result: ', value);
+				resolve(value);
+			});
+		});
+	});
 }
 
 
