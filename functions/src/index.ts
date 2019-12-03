339832c2f5984130c644799f1410cbf9b346b579
Larbi Gharib <larbi.gharib@savoirfairelinux.com>
2019-12-02 21:25:50 -0500

    [ADD] Added more intents

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -27,21 +27,53 @@ const odoo = new Odoo({
 });
 
 
-// Capture Intent
-app.intent('The name of the lead is', async (conv, {any}) => {
-	return createLead(String(any)).then(function(data) {
+app.intent('The name of the lead - probability - no', async (conv, {lead_name, percentage}) => {
+	return createLead(String(lead_name), 
+	String(percentage).replace('%', ''), 
+	'', ''
+	).then(function(data) {
 		conv.close(new SimpleResponse({
-			text: `Lead ${any} created`,
-			speech: `The lead ${any} was created`, 
+			text: `Lead ${lead_name} created`,
+			speech: `The lead ${lead_name} was created with ${String(percentage).replace('%', '')}% of chance to win it.`, 
 		}));
 		conv.close(new BasicCard({
-				text: `Lead ${any} created`,
+				text: `Lead ${lead_name} created`,
 				image: new Image({
 				url: `https://kioteservices.com/wp-content/uploads/2017/12/odoo_logo.png`,
 				alt: `Odoo Logo`,
 			}),
 			buttons: new Button({
-				title: `Open ${any} lead`,
+				title: `Open ${lead_name} lead`,
+				url: `https://lgharib-odoo-assistant.odoo.com/web?#id=${String(data)}&action=168&model=crm.lead&view_type=form&cids=1&menu_id=121`,
+			})
+		}));
+	}).catch(function(){
+		conv.close(new SimpleResponse({
+			text: `Error`,
+			speech: `Error`, 
+		}));
+	});
+		
+});
+
+app.intent('The name of the lead - probability - yes - contact name - no', async (conv, {lead_name, percentage, contact_name}) => {
+	return createLead(String(lead_name), 
+	String(percentage).replace('%', ''), 
+	String(contact_name),
+	''
+	).then(function(data) {
+		conv.close(new SimpleResponse({
+			text: `Lead ${lead_name} created`,
+			speech: `The lead ${lead_name} was created with ${String(percentage).replace('%', '')}% of chance to win it.`, 
+		}));
+		conv.close(new BasicCard({
+				text: `Lead ${lead_name} created`,
+				image: new Image({
+				url: `https://kioteservices.com/wp-content/uploads/2017/12/odoo_logo.png`,
+				alt: `Odoo Logo`,
+			}),
+			buttons: new Button({
+				title: `Open ${lead_name} lead`,
 				url: `https://lgharib-odoo-assistant.odoo.com/web?#id=${String(data)}&action=168&model=crm.lead&view_type=form&cids=1&menu_id=121`,
 			})
 		}));
@@ -56,17 +88,28 @@ app.intent('The name of the lead is', async (conv, {any}) => {
 
 
 // Helper Function for scrapping a webpage
-function createLead(any:string) {
+function createLead(lead_name:string, percentage:string, contact_name:string, phone_number:string) {
+	let new_percentage = percentage;
+	if(Number(percentage)>100){
+		new_percentage = '100;';
+	}else if(Number(percentage)<0){
+		new_percentage = '0';
+	}
 	return new Promise((resolve, reject)=>{
 		odoo.connect(  function (error:any) {
-			if (error) { console.log(error); }
+			if (error) { reject(); }
 			console.log('Connected to Odoo server.');
 			const inParams = [];
-			inParams.push({'name': any})
+			inParams.push({
+				'name': lead_name, 
+				'probability': new_percentage,
+				'contact_name': contact_name,
+				'phone': phone_number
+			});
 			const params = [];
 			params.push(inParams);
 			odoo.execute_kw('crm.lead', 'create', params, function (err:any, value:any) {
-				if (err) { console.log(err); }
+				if (err) { reject(); }
 				console.log('Result: ', value);
 				resolve(value);
 			});
